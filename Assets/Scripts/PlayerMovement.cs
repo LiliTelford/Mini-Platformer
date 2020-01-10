3cc41551e49636e8ffafb1e1b66142974d8a4e17
Lili <lili.telford@hotmail.co.uk>
2020-01-10 16:24:31 +0000

    adding movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.GetAxisRaw("Horizontal"));
+
+        Rigidbody2D player = GetComponent<Rigidbody2D>();
+        float speed = 5f;
+
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.position += new Vector3(1, 0) * speed * Time.deltaTime;
+            animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.position += new Vector3(-1, 0) * speed * Time.deltaTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
