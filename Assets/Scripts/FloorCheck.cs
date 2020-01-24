3462b3b7bb2204de32c9f44489195deb2fd49674
Lili <lili.telford@hotmail.co.uk>
2020-01-24 20:40:48 +0000

    Added jumping

---
+++ Assets/Scripts/FloorCheck.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorCheck : MonoBehaviour
+{
+    GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Floor")
+        {
+            Player.GetComponent<PlayerMovement>().isOnFloor = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Floor")
+        {
+            Player.GetComponent<PlayerMovement>().isOnFloor = false;
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,28 +5,48 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Animator animator;
+    public float speed = 5f;
+    public float jumpHeight = 5f;
+    public bool isOnFloor = false;
 
     // Update is called once per frame
     void Update()
     {
         bool runRightBool = false;
+        bool runLeftBool = false;
 
+        //tells us if either the A or D keys are being pressed
         Debug.Log(Input.GetAxisRaw("Horizontal"));
 
-        Rigidbody2D player = GetComponent<Rigidbody2D>();
-        float speed = 5f;
+        Rigidbody2D Player = GetComponent<Rigidbody2D>();
 
+        //checks if the player is holding down the D key
         if (Input.GetAxisRaw("Horizontal") > 0)
         {
+            //moves the player character right
             transform.position += new Vector3(1, 0) * speed * Time.deltaTime;
             runRightBool = true;
         }
+        //checks if the player is holding down the A key
         if (Input.GetAxisRaw("Horizontal") < 0)
         {
+            //moves the player character left
             transform.position += new Vector3(-1, 0) * speed * Time.deltaTime;
+            runLeftBool = true;
         }
 
+        if(Input.GetKeyDown("space") && isOnFloor == true)
+        {
+            Player.AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+        }
+        //sets the animation bools as true or false depending if the A or D keys are being held down
         animator.SetBool("runRightBool", runRightBool);
+        animator.SetBool("runLeftBool", runLeftBool);
+    }
+
+    void Jump()
+    {
+
     }
 
     void FixedUpdate()
